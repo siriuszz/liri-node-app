a917da0cf3417dc9100b4aacca47674ee3714a73
Star Lee <zabra15@yahoo.com>
2017-10-20 17:36:35 -0400

    twitter, omdb working

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var twitterKeys = require("./keys");
+var spotifyKeys = require("./spotKey");
 var twitter = require('twitter');
 var spotify = require("node-spotify-api");
 var request = require("request");
@@ -8,7 +9,7 @@ var fs = require("fs");
 // User inputs
 var action = process.argv[2];
 var selection = process.argv[3];
-var myTweets = "";
+var movie = "";
 
 
 // Use input to determine which action to take
@@ -29,14 +30,14 @@ function getInput() {
 // Retrieve tweets from Twitter
 function getTweets() {
 
-    for (var key in twitterKeys) {
-        var client = new twitter({
-            consumer_key: twitterKeys.consumer_key,
-            consumer_secret: twitterKeys.consumer_secret,
-            access_token_key: twitterKeys.access_token_key,
-            access_token_secret: twitterKeys.access_token_secret
-        });
-    }
+    var client = new twitter({
+        consumer_key: twitterKeys.consumer_key,
+        consumer_secret: twitterKeys.consumer_secret,
+        access_token_key: twitterKeys.access_token_key,
+        access_token_secret: twitterKeys.access_token_secret
+    });
+
+
 
     var params = {screen_name: 'jutahfbi', count: 20};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -54,18 +55,56 @@ function getTweets() {
 
 
 // Retrieve song info from Spotify
+// =============== not working ===================
 function getSong() {
 
-    var spotifySong = new spotify;
-    spotifySong.search({ type: 'track', query: selection }, function(err, data) {
+    var spotifySong = new spotify({
+        id: spotifyKeys.id,
+        secret: spotifyKeys.secret
+    });
+
+    spotifySong.search({ type: 'track', query: selection, limit: 1 }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         } else {
-            console.log(data);
+            console.log(data.tracks.items[0].name + data.tracks.items[0].name);
         }
 
     });
 }
 
+
+// Retrieve movie info from OMDB
+function getMovie() {
+    var queryUrl = "http://www.omdbapi.com/?t=" + selection + "&y=&plot=short&apikey=40e9cece";
+
+    request(queryUrl, function(error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            console.log("Title: " + JSON.parse(body).Title +
+            "\nRelease Year: " + JSON.parse(body).Year +
+            "\nRating: " + JSON.parse(body).Rated +
+            "\nCountry: " + JSON.parse(body).Country +
+            "\nLanguage: " + JSON.parse(body).Language +
+            "\nPlot: " + JSON.parse(body).Plot +
+            "\nActors: " + JSON.parse(body).Actors)
+        }
+    });
+}
+
+
+// Get text from random.txt
+//=======================not working==============================
+function getText() {
+    fs.readFile("random.text", "utf-8", function (error, data) {
+        if(error) {
+            console.log(error);
+        } else {
+            var fromFile = data.split(", ");
+            action = fromFile[0];
+            selection = fromFile[1];
+        }
+    })
+}
 
 getInput();
---
+++ spotKey.js
@@ -0,0 +1,6 @@
+var spotifyKeys = {
+    id: 'd56368cb627b40caaa223213fc90d106',
+    secret: 'b65458832dbf439ca905f98922f19a59'
+};
+
+module.exports = spotifyKeys;
